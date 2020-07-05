dacd634f100035814e7aa5ca3edd8ef29612f6a7
yum-feng <yum_feng@encs.concordia.ca>
2020-07-05 13:52:44 -0400

    begin assignment3

---
+++ assignment3/Menu.hpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+class Menu
+{
+  std::vector<std::string> option_list;
+  std::string top_msg;
+  std::string bottom_msg;
+
+public:
+  Menu();
+
+  void insert( int index, std::string& option );
+
+  void push_back(std::string option);
+
+  void remove(int index);
+
+  int size() const;
+
+  void pop_back();
+
+  std::string get(int k);
+
+  std::string toString() const;
+
+  int read_option_number();
+  
+  void set_top_message(const std::string &m);
+  void set_bottom_message(const std::string &m);
+
+  void clear_top_message();
+  void clear_bottom_message();
+
+  bool isEmpty() const;
+  
+  
+  remove
+};
